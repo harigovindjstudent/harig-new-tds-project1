c7acb96412088b46330423be758a3e5e51828492
harigovindjstudent <23f2004143@ds.study.iitm.ac.in>
2025-06-17 21:38:43 +0530

    uvicorn update last line

--- app.py
+++ app.py
@@ -132,4 +132,4 @@ async def query_api(req: QueryRequest):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app",reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
